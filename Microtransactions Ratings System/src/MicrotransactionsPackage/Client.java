c5169e68104aad1086a6e01ba08d66aca9a9d878
michaelma4 <michaelmarino8@msn.com>
2018-11-28 21:28:57 -0500

    updated client with GUI-Intro

--- Microtransactions Ratings System/src/MicrotransactionsPackage/Client.java
+++ Microtransactions Ratings System/src/MicrotransactionsPackage/Client.java
@@ -3,15 +3,23 @@ package MicrotransactionsPackage;
 //Java implementation for a client 
 //Save file as Client.java 
 
+import java.util.concurrent.TimeUnit;
 import java.io.*; 
 import java.net.*; 
 import java.util.Scanner; 
-import javafx.application.Application; 
+import javafx.application.Application;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;  // HBox class in here
+import javafx.scene.text.Text;
 import javafx.scene.image.ImageView; 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 
 //Client class 
 public class Client extends Application {  
@@ -35,9 +43,80 @@ public class Client extends Application {
 		Scene s = new Scene(p);
 
 		// place pane on stage
-		primaryStage.setTitle("Welcome to SKG's Game Rating System!");
+		primaryStage.setTitle("Welcome to SKG's Game Rating System! Please wait while this loads.");
         primaryStage.setScene(s);
      	primaryStage.show();
+     	
+    	
+     		//Show the main page at this point
+     		
+     		TextField nameTextField = new TextField();
+     		
+			Button createButton = new Button("Create");
+			Button lookupButton = new Button("Lookup");
+			Button exitButton = new Button("Exit");
+			
+			GridPane g = new GridPane();
+	    	g.setHgap(10);
+	    	g.setVgap(10);
+			
+	    	g.add(new Label("Enter the game name"), 0, 0);
+	    	g.add(nameTextField, 1,0);
+	    	g.add(createButton, 0, 3);
+	    	g.add(lookupButton, 0, 4);	    	
+	    	g.add(exitButton, 0, 5);
+	    	g.add(image1, 0, 6);
+	    	g.setAlignment(Pos.CENTER);
+	    	
+			Scene scene = new Scene(g,1200,400);
+			primaryStage.setTitle("SKG - Game Ratings System");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+				createButton.setOnAction(e ->{			
+												
+				String gameName = nameTextField.getText();
+				
+					
+				//makes a screen, and makes it the size of user's screen, so i can make next scene size of screen
+				Screen screen = Screen.getPrimary();
+				Rectangle2D bounds = screen.getVisualBounds();
+				
+			//	g.add(info, 0, 0);
+				primaryStage.setX(bounds.getMinX());
+				primaryStage.setY(bounds.getMinY());
+				primaryStage.setWidth(bounds.getWidth());
+				primaryStage.setHeight(bounds.getHeight());
+				primaryStage.setTitle("Create Command");				
+				primaryStage.show();
+				
+			});
+				
+				lookupButton.setOnAction(e ->{			
+					
+					String gameName = nameTextField.getText();
+					String infoString  = ("The game name is: " + gameName);
+					Text info = new Text(infoString);
+					
+					g.add(info, 1, 3);
+					//makes a screen, and makes it the size of user's screen, so i can make next scene size of screen
+					Screen screen = Screen.getPrimary();
+					Rectangle2D bounds = screen.getVisualBounds();
+					
+					
+					primaryStage.setX(bounds.getMinX());
+					primaryStage.setY(bounds.getMinY());
+					primaryStage.setWidth(bounds.getWidth());
+					primaryStage.setHeight(bounds.getHeight());
+					primaryStage.setTitle("Lookup Command");				
+					primaryStage.show();
+					
+				});
+				
+				exitButton.setOnAction(e ->{			
+					
+				});
+     	
 	}
 
  public static void main(String[] args) throws IOException { 
@@ -49,7 +128,7 @@ public class Client extends Application {
          Scanner scn = new Scanner(System.in); 
            
          // getting localhost ip 
-         InetAddress ip = InetAddress.getByName("localhost"); 
+         InetAddress ip = InetAddress.getByName("localhost"); //ip is ip address of server goes here
    
          // establish the connection with server port 5056 
          Socket s = new Socket(ip, 6789); 
